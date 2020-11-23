09a12f142ead7435b8a65f0edbd1bcc6d5df91e9
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-11-23 16:32:32 -0300

    adding dangerouslySetInnerHTML for GTM module

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import HubspotForm from 'react-hubspot-form';
+import Head from 'next/head';
 
 
 export default function Home() {
-
+  const renderGTMSnippet = () => (
+    <script 
+      dangerouslySetInnerHTML={{
+        __html:`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MRL9KJ7')
+        `
+      }}
+    />
+  )
   return (
     <>
+      <Head>
+        {renderGTMSnippet()}
+        <title>Teste infra</title>
+      </Head>
       <h1>Home</h1>
       <HubspotForm
         portalId='6331207'
